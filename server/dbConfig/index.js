753f69d79208fdbb10343ef681a385a414c15c1b
Djaffar <d_tadjer@estin.dz>
2024-11-13 22:05:54 +0100

    Modify the DB connection

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -1,19 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Connect = () => {
-    // MongoDB connection URI
-const mongoURI = 'mongodb://localhost:27017/E_learn_database';
+  const mongoURI = process.env.MONGO_URI || 'mongodb://db:27017/E_learn_database';
 
-// Connect to MongoDB
-mongoose.connect(mongoURI);
+  // Connect to MongoDB without deprecated options
+  mongoose.connect(mongoURI);
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
 
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
+  mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+  });
 }
 
-module.exports = Connect
+module.exports = Connect;
